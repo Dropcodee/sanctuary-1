2bb840316894cd18b832adeb4ad092a06f9c182d
Joshua-owolabi <primtech.co@gmail.com>
2018-10-21 19:51:23 +0100

    Edited the main.js and the login.html

--- view/dist/js/main.js
+++ view/dist/js/main.js
@@ -13,27 +13,46 @@ $(document).ready(function() {
 });
 
 $(function() {
-  const users = {
-    name: "Abraham Ugbeshe",
-    reg_no: "1700172"
-  };
-  $(document).on("click", ".change_text", function() {
-    $(".change_text").html("<img src='image/loader.gif' />");
-    $.ajax({
-      method: "GET",
-      url: "http://dos.com/users",
-      data: { users },
-      cache: false,
-      success: function() {
-        $(".show").html(users.name);
+  $(document).on("click", "#login", function(e) {
+    e.preventDefault();
+    let reg_no = $("#RegNo").val();
+    let password = $("#password").val();
 
-        $(".change_text").html("Changed");
-      },
-      error: function() {
-        $(".show").html("Sorry couldn't connect to the server");
+    if (reg_no === "" || password === "") {
+      let err = {
+        password: "Password field required",
+        reg_no: "Registration Field required"
+      };
+      $(".reg_err").html(err.reg_no);
+      $(".pass_err").html(err.password);
+    } else {
+      $("#login").html("Loading...");
+      $.ajax({
+        method: "POST",
+        url: "https://sanc-server.herokuapp.com/public/login",
+        data: { reg_no: reg_no, password: password },
+        cache: false,
+        success: function(data) {
+          var data_login = jQuery.parseJSON(data);
+          if (data_login.error) {
+            console.log(data_login.error);
+            $(".show_err").html(data_login.error.err_text);
+            $("#login").html("Login");
+          } else {
+            console.log(data_login.success);
+            $(".show_success").html(data_login.success.success_text);
+            $(".show_err").html("");
+            window.location = "index.html";
+          }
+        },
+        error: function(err) {
+          $(".show_err").html(
+            "Sorry Sanctuary Unit couldn't connect to the server"
+          );
 
-        $(".change_text").html("No change");
-      }
-    });
+          $("#login").html("Login");
+        }
+      });
+    }
   });
 });
